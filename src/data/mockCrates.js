184548029d4259bc4f6eca1456743f2d809578fc
Daniel Kolpakov <74573474+danielkolpakov00@users.noreply.github.com>
2026-04-27 23:20:44 -0700

    updated favicon

--- src/data/mockCrates.js
+++ src/data/mockCrates.js
@@ -1,3 +1,9 @@
-const mockCrates = []
+const mockCrates = [
+  {
+    id: 'playlist-gems',
+    name: 'Gems',
+    description: 'Special holding area for the best finds.',
+  },
+]
 
 export default mockCrates
